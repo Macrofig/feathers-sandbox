d32ec167eb965cac53875572d65b9496f0053b7e
System <none>
2017-05-11 01:19:07 +0000

    Added hook that coerces string to boolean

--- server.js
+++ server.js
@@ -2,6 +2,52 @@ const feathers = require('feathers');
 const bodyParser = require('body-parser');
 const rest = require('feathers-rest');
 const memory = require('feathers-memory');
+const hooks = require('feathers-hooks');
+
+'use strict';
+
+
+
+const serviceHooks = {
+	before: {
+		all: [hook => {
+      console.log(hook.params.query);
+			if (typeof hook.params.query.read !== 'undefined') {
+				hook.params.query.read = (hook.params.query.read === 'true') ? true : false;
+			}
+      console.log(hook.params.query);
+      return hook;
+		}],
+		find: [],
+		get: [],
+		create: [],
+		update: [],
+		patch: [],
+		remove: []
+	},
+
+	after: {
+		all: [],
+		find: [],
+		get: [],
+		create: [],
+		update: [],
+		patch: [],
+		remove: []
+	},
+
+	error: {
+		all: [],
+		find: [],
+		get: [],
+		create: [],
+		update: [],
+		patch: [],
+		remove: []
+	}
+};
+
+
 
 // Create a feathers instance.
 const app = feathers()
@@ -10,7 +56,8 @@ const app = feathers()
   // Turn on JSON parser for REST services
   .use(bodyParser.json())
   // Turn on URL-encoded parser for REST services
-  .use(bodyParser.urlencoded({ extended: true }));
+  .use(bodyParser.urlencoded({ extended: true }))
+  .configure(hooks());
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
@@ -25,8 +72,10 @@ app.use('/messages', memory({
   }
 }));
 
+const messageService = app.service('messages');
+messageService.hooks(serviceHooks);
 // Create a dummy Message
-app.service('messages').create({
+messageService.create({
   text: 'Server message',
   read: false
 }).then(function(message) {
